ed122eba959407f99a4d4d80c8bccc6f4b1a132f
carole-ouedraogo <ouedca@gmail.com>
2020-07-02 14:02:48 -0400

    first commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,33 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, {useState} from 'react';
+import ReactDOM from 'react-dom';
+import logo from "./logo.svg";
+import './App.css'
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    const [count, setCount] = useState(0);
+    return (
+
+    <header className="App-header">
+
+      <div id = "top">
+      <img src={logo} className="App-logo" alt="logo" />
+      <h1>My Counter App</h1>
+      <br></br>
+      </div>
+    
+        <div>
+            <div className="displayedNumber">{count}</div>
+            <button type="button" class="btn btn-outline-primary" onClick={() => setCount(count + 1)}>Add to Count</button>
+        </div>
+          <br></br>
+    </header>
+    );
+
 }
+
+ReactDOM.render(
+    <App/>,
+    document.getElementById('root')
+);
 
 export default App;
